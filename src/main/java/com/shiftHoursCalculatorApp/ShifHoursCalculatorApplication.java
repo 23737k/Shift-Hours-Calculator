2905891b100a7f1a732521947660d230050fc4ff
kenti <016mki69@gmail.com>
2024-07-02 20:49:56 -0300

    Agrego cron

--- src/main/java/com/shiftHoursCalculatorApp/ShifHoursCalculatorApplication.java
+++ src/main/java/com/shiftHoursCalculatorApp/ShifHoursCalculatorApplication.java
@@ -1,5 +1,14 @@
 package com.shiftHoursCalculatorApp;
 
+import com.shiftHoursCalculatorApp.cron.MyJob;
+import org.quartz.CronScheduleBuilder;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +17,29 @@ public class ShifHoursCalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShifHoursCalculatorApplication.class, args);
+
+		try {
+			// Creamos una instancia del scheduler
+			Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+
+			// Comenzamos el scheduler
+			scheduler.start();
+
+			// Definimos el job
+			JobDetail job = JobBuilder.newJob(MyJob.class).withIdentity("myJob", "group1").build();
+
+			// Definimos el trigger que ejecutará el trabajo cada 14 minutos
+			Trigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger1", "group1")
+					.withSchedule(CronScheduleBuilder.cronSchedule("0 */14 * * * ?")).build();
+
+			// Programamos el job con el trigger
+			scheduler.scheduleJob(job, trigger);
+
+		} catch (SchedulerException e) {
+			e.printStackTrace();
+    }
+
+  }
 	}
 
-}
+
