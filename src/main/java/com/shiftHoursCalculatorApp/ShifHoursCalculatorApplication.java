1f341f819920fe6cde178ab5334f1b36643d9c3d
kenti <016mki69@gmail.com>
2024-07-02 19:37:50 -0300

    First commit

---
+++ src/main/java/com/shiftHoursCalculatorApp/ShifHoursCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.shiftHoursCalculatorApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShifHoursCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShifHoursCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shiftHoursCalculatorApp/ShifHoursCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiftHoursCalculatorApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShifHoursCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
